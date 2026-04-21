ee594ce8f9f87ba0da8beacbe65bed4ac6bfd7f0
Saurav Raj <rajsaurav803114@gmail.com>
2026-04-22 01:16:37 +0530

    removed root mapping

--- src/main/java/com/example/complaintsystem/HomeController.java
+++ src/main/java/com/example/complaintsystem/HomeController.java
@@ -1,13 +1,10 @@
 package com.example.complaintsystem;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Complaint System Running Successfully 🚀";
-    }
+   
 }
